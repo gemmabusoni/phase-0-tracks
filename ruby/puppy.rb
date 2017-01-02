7cca8445c10b63c7347cd73dbbf12346bf1d452b
Gemma Busoni <grbusoni@gmail.com>
2017-01-02 08:03:13 -0800

    Puppy behavior

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+=begin
+class Puppy
+SPECIES ------------------------
+Canine
+CHARACTERISTICS ----------------
+Eyelash length: curly
+Friendly: somtimes
+Eye count: 2
+Name: varies
+Color: varies
+BEHAVIOR -----------------------
+fetch
+roll over
+high five
