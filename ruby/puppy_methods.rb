8ea56d28672f722cf892b6f0bc8edcf23b9e6943
Gemma Busoni <grbusoni@gmail.com>
2017-01-02 08:10:24 -0800

    puppy methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,71 @@
-class Puppy
+##Release 0
+#class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
+#    def initialize 
+#      puts "Initializing new puppy instance ..."
+#    end
+#  
+#  def fetch(toy)
+#    puts "I brought back the #{toy}!"
+#    toy
+#  end
+#
+#  def speak(number)
+#    number.times do |x|
+#    puts "Woof!"
+#    end
+#  end
+#  
+#  def roll_over(command)
+#    puts "roll over #{command}"
+#  end
+#  
+#  def dog_years(age)
+#    age.to_i * 7
+#  end
+#  
+#  def shake(command)
+#    puts "shake #{command}"
+#  end
+#  
+#end
+#
+#puppy1 = Puppy.new
+##Driver code
+#puppy1.fetch("ball")
+#p puppy1.speak(5)
+#p puppy1.roll_over("Doggy")
+#p puppy1.dog_years(2)
+#p puppy1.shake("Doggy")
+
+#Release 2
+#Creating new class
+class Soccer
+  def initialize 
+    puts "you created a Soccer instance!!"
   end
-
+  
+  def Goal
+    puts "GOOO-AAAAAA-LLLLL!!"
+  end
+  
+  def miss
+    puts "booo"
+  end
+  
+  
 end
+#Container for instances
+basketball_array = []
 
+50.times do
+player1 = Soccer.new
+player1
+soccer_array << player1
+end
+puts soccer_array.length
+#Iterating everything inside of the soccer array
+soccer_array.each do |instances| 
+  instances.Goal
+  instances.miss
+end
